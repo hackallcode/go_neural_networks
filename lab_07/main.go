6a05e777d43c4fe006511148b57a2a4549d48539
Vladimir <vsev.s@mail.ru>
2019-05-03 01:54:09 +0300

    examples fixed

--- lab_07/main.go
+++ lab_07/main.go
@@ -58,87 +58,115 @@ var (
 )
 
 func main() {
-    // rnn123 := rnn.CreateRNN([][]int{letter1, letter2, letter3}, 15, 100)
-    // fmt.Println("\n123 weights:")
-    // rnn123.PrintWeights()
+    fmt.Println()
+    rnn123 := rnn.CreateRNN([][]int{letter1, letter2, letter3}, 15, 100)
+    fmt.Println("123 weights:")
+    rnn123.PrintWeights()
+
+    fmt.Println()
+    res1 := rnn123.Detect(letter1, true, 1)
+    fmt.Println("Letter 1:")
+    rnn.PrintBWLetterByVector(res1, 3, 5)
+
+    fmt.Println()
+    res2 := rnn123.Detect(letter2, true, 1)
+    fmt.Println("Letter 2:")
+    rnn.PrintBWLetterByVector(res2, 3, 5)
+
+    fmt.Println()
+    res3 := rnn123.Detect(letter3, true, 1)
+    fmt.Println("Letter 3:")
+    rnn.PrintBWLetterByVector(res3, 3, 5)
+
+    fmt.Println()
+    resBad1 := rnn123.Detect(rnn.BWLetterToVector([]string{
+        "** ",
+        " * ",
+        " * ",
+        " * ",
+        " * ",
+    }, 3, 5), true, 1)
+    fmt.Println("Bad letter 1:")
+    rnn.PrintBWLetterByVector(resBad1, 3, 5)
+
+    fmt.Println()
+    resBad2 := rnn123.Detect(rnn.BWLetterToVector([]string{
+        " **",
+        "   ",
+        "***",
+        "*  ",
+        "** ",
+    }, 3, 5), true, 1)
+    fmt.Println("Bad letter 2:")
+    rnn.PrintBWLetterByVector(resBad2, 3, 5)
+
+    fmt.Println()
+    resBad3 := rnn123.Detect(rnn.BWLetterToVector([]string{
+        " * ",
+        "  *",
+        " **",
+        "  *",
+        " **",
+    }, 3, 5), true, 1)
+    fmt.Println("Bad letter 3:")
+    rnn.PrintBWLetterByVector(resBad3, 3, 5)
+
+    // fmt.Println()
+    // rnnSTU := rnn.CreateRNN([][]int{letterS, letterT, letterU}, 35, 100)
+    // fmt.Println("STU weights:")
+    // rnnSTU.PrintWeights()
     //
-    // res1 := rnn123.Detect(rnn.BWLetterToVector([]string{
-    //     " * ",
-    //     " * ",
-    //     "** ",
-    //     " * ",
-    //     "***",
-    // }, 3, 5), true, 0)
-    // fmt.Println("\n1:")
-    // rnn.PrintBWLetterByVector(res1, 3, 5)
+    // fmt.Println()
+    // resS := rnnSTU.Detect(letterS, false, 0)
+    // fmt.Println("Letter S:")
+    // rnn.PrintBWLetterByVector(resS, 5, 7)
     //
-    // res2 := rnn123.Detect(letter2, true, 0)
-    // fmt.Println("\n2:")
-    // rnn.PrintBWLetterByVector(res2, 3, 5)
+    // fmt.Println()
+    // resT := rnnSTU.Detect(letterT, false, 0)
+    // fmt.Println("Letter T:")
+    // rnn.PrintBWLetterByVector(resT, 5, 7)
     //
-    // res3 := rnn123.Detect(letter3, true, 0)
-    // fmt.Println("\n3:")
-    // rnn.PrintBWLetterByVector(res3, 3, 5)
+    // fmt.Println()
+    // resU := rnnSTU.Detect(letterU, false, 0)
+    // fmt.Println("Letter U:")
+    // rnn.PrintBWLetterByVector(resU, 5, 7)
     //
-    // res23 := rnn123.Detect(rnn.BWLetterToVector([]string{
-    //     "***",
-    //     "   ",
-    //     "***",
-    //     "   ",
-    //     "***",
-    // }, 3, 5), true, 0)
-    // fmt.Println("\nBad 2:")
-    // rnn.PrintBWLetterByVector(res23, 3, 5)
-
-    rnnSTU := rnn.CreateRNN([][]int{letterS, letterT, letterU}, 35, 100)
-    fmt.Println("\nSTU weights:")
-    rnnSTU.PrintWeights()
-
-    resS := rnnSTU.Detect(letterS, false, 0)
-    fmt.Println("\nS:")
-    rnn.PrintBWLetterByVector(resS, 5, 7)
-
-    resT := rnnSTU.Detect(letterT, false, 0)
-    fmt.Println("\nT:")
-    rnn.PrintBWLetterByVector(resT, 5, 7)
-
-    resU := rnnSTU.Detect(letterU, false, 0)
-    fmt.Println("\nU:")
-    rnn.PrintBWLetterByVector(resU, 5, 7)
-
-    resBadS := rnnSTU.Detect(rnn.BWLetterToVector([]string{
-        "     ",
-        " *** ",
-        "*    ",
-        " *** ",
-        "    *",
-        "    *",
-        " *** ",
-    }, 5, 7), false, 0)
-    fmt.Println("\nBad S:")
-    rnn.PrintBWLetterByVector(resBadS, 5, 7)
-
-    resBadT := rnnSTU.Detect(rnn.BWLetterToVector([]string{
-        "     ",
-        "     ",
-        " *** ",
-        "  *  ",
-        "  *  ",
-        "     ",
-        "     ",
-    }, 5, 7), false, 0)
-    fmt.Println("\nBad T:")
-    rnn.PrintBWLetterByVector(resBadT, 5, 7)
-
-    resBadU := rnnSTU.Detect(rnn.BWLetterToVector([]string{
-        "     ",
-        "     ",
-        "*   *",
-        "*   *",
-        "*   *",
-        " *** ",
-        "     ",
-    }, 5, 7), false, 0)
-    fmt.Println("\nBad U:")
-    rnn.PrintBWLetterByVector(resBadU, 5, 7)
+    // fmt.Println()
+    // resBadS := rnnSTU.Detect(rnn.BWLetterToVector([]string{
+    //     "     ",
+    //     " *** ",
+    //     "*    ",
+    //     " *** ",
+    //     "    *",
+    //     "    *",
+    //     " *** ",
+    // }, 5, 7), false, 0)
+    // fmt.Println("Bad letter S:")
+    // rnn.PrintBWLetterByVector(resBadS, 5, 7)
+    //
+    // fmt.Println()
+    // resBadT := rnnSTU.Detect(rnn.BWLetterToVector([]string{
+    //     "     ",
+    //     "     ",
+    //     " *** ",
+    //     "  *  ",
+    //     "  *  ",
+    //     "     ",
+    //     "     ",
+    // }, 5, 7), false, 0)
+    // fmt.Println("Bad letter T:")
+    // rnn.PrintBWLetterByVector(resBadT, 5, 7)
+    //
+    // fmt.Println()
+    // resBadU := rnnSTU.Detect(rnn.BWLetterToVector([]string{
+    //     "     ",
+    //     "     ",
+    //     "*   *",
+    //     "*   *",
+    //     "*   *",
+    //     " *** ",
+    //     "     ",
+    // }, 5, 7), false, 0)
+    // fmt.Println("Bad letter U:")
+    // rnn.PrintBWLetterByVector(resBadU, 5, 7)
 }
